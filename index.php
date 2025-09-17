7e9b6c10e9f7ba1aaefc0e6cf1b193051577fcc8
Elshara Silverheart <sourceminded@gmail.com>
2025-09-17 09:38:27 -0700

    Load optional Composer autoloader before setup detection

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 define('NF_APP_BASE', __DIR__);
 
+$autoloadPath = NF_APP_BASE . '/vendor/autoload.php';
+if (is_file($autoloadPath)) {
+    require_once $autoloadPath;
+}
+
 $configPath = NF_APP_BASE . '/config/app.php';
 if (!is_file($configPath)) {
     require NF_APP_BASE . '/setup/index.php';
